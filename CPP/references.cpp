a6d79d492e0c0d2e98e1a89bdef5f5a3657896ef
nandu506tvm <nandu506tvm@gmail.com>
2023-01-29 10:25:57 +0530

    References in C++ sample code

---
+++ CPP/references.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int updateValue(int &value) {
+    return (value + 1);
+}
+
+int main(int argc, char const *argv[]) {
+
+    int a = 10;
+    cout << "Initial Value: " << a << endl;
+    cout << "Final Value: " << updateValue(a) << endl;
+
+    return 0;
+}
+
