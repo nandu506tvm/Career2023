ed3b621075ef99880f6e8fd6be1dfdf4b57a6e37
nandu506tvm <nandu506tvm@gmail.com>
2023-02-05 09:29:24 +0530

    vector - C++ std 11 fixes

--- CPP/vectors.cpp
+++ CPP/vectors.cpp
@@ -32,7 +32,11 @@ double average(vector<int> & salary) {
 
 int main(int argc, char const *argv[]) {
 
-    vector<int> salary= {4000,3000,1000,2000};
+    vector<int> salary;
+    salary.push_back(4000);
+    salary.push_back(3000);
+    salary.push_back(1000);
+    salary.push_back(2000);
     cout << average(salary) << endl;
 
     return 0;
