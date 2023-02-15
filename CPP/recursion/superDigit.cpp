6feb9199f855a75991e6b86e769365c1b60b8402
nandu506tvm <nandu506tvm@gmail.com>
2023-02-15 10:11:31 +0530

    Recursive digit sum - Hackerrank Solution

---
+++ CPP/recursion/superDigit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int superDigit(string S, int k)
+{
+    long result = 0;
+
+    // Base condition 
+    if (S.length() == 1 ) return stoi(S);
+
+    // Iterate throught the string and convert it into int
+    for (char &c: S) {
+        result += c - 48;
+    }
+
+    result *= k;
+
+    // This is needed to calculate if result has more than two digits. That is to add digits of 39
+    if (result%10 != result) {
+        result = superDigit(to_string(result),1);
+    }
+
+    return result;
+}
+
+int main(int argc, char const *argv[]) {
+
+    string N = "";
+    int K = 0;
+    cout << "Enter string: ";
+    cin >>  N;
+    cout << "Enter Value of K: ";
+    cin >> K;
+    cout << superDigit(N,K) << endl;
+    
+    return 0;
+}
+
