3a151e33b36b5f2a6d368e621ee75225d0819e92
nandu506tvm <nandu506tvm@gmail.com>
2023-02-17 13:59:26 +0530

    Binary search -  code added

--- CPP/algorithms/binarySearch.cpp
+++ CPP/algorithms/binarySearch.cpp
@@ -6,7 +6,26 @@ using namespace std;
 bool binarySearch(vector <int> &vect, int leftIndex, int rightIndex, int key)
 {
     bool isElementPresent = false;
+
+    //Base condition
+    if (leftIndex > rightIndex) return false;
+
     int mid = (leftIndex + rightIndex)/2;
+
+    // Check if key is present at mid
+    if (vect[mid] == key ) {return true;}
+
+    // Check left array if key is less than value at index 'mid'
+    else if (key < vect[mid])
+    {
+        isElementPresent = binarySearch(vect, leftIndex, mid-1, key);
+    }
+
+    // Else check right array
+    else
+    {
+        isElementPresent = binarySearch(vect, mid+1, rightIndex, key);
+    }
 
     return isElementPresent;
 }
