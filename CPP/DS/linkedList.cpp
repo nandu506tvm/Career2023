377621f1bf863914c4a280c55e9a4ef173279857
Nandu J B <80139351+nandu506tvm@users.noreply.github.com>
2023-03-11 14:33:19 +0530

    Update linkedList.cpp
    Space-Tab issues on github

--- CPP/DS/linkedList.cpp
+++ CPP/DS/linkedList.cpp
@@ -19,43 +19,43 @@ void LinkedList :: pushFront (int data, Node **head, Node **tail)
 
 void LinkedList :: pushBack (int data, Node **head, Node **tail) 
 {
-	if (*head == NULL) 
+    if (*head == NULL) 
     {
-		pushFront (data, head, tail);
-		return;
-	}
+        pushFront (data, head, tail);
+	return;
+    }
 
-	Node *new_node = new Node (data);
+    Node *new_node = new Node (data);
 
     // Make the existing tail node point to new node
-	(*tail)-> next = new_node;
-	*tail = new_node;
+    (*tail)-> next = new_node;
+    *tail = new_node;
 }
 
 void LinkedList :: printList (Node *current_node) 
 {
-	if (current_node == NULL) 
+    if (current_node == NULL) 
     {
         cout << endl;
         return;
     }
 
-	cout << current_node->data << "\t";
-	printList (current_node -> next);
+    cout << current_node->data << "\t";
+    printList (current_node -> next);
 }
 
 int LinkedList :: findLength (Node *current_node) 
 {
-	if (current_node == NULL) return 0;
+    if (current_node == NULL) return 0;
 
-	return (1 + findLength (current_node->next));
+    return (1 + findLength (current_node->next));
 }
 
 bool LinkedList :: searchkey (Node *current_node, int key) 
 {
-	if (current_node == NULL) return false;
-	if (current_node -> data == key) return true;
-	return searchkey (current_node -> next, key);
+    if (current_node == NULL) return false;
+    if (current_node -> data == key) return true;
+    return searchkey (current_node -> next, key);
 }
 
 void LinkedList :: popFront(Node **head, Node **tail) 
@@ -141,15 +141,15 @@ void LinkedList :: deleteList (Node **head, Node **tail)
 
 int main () 
 {
-	LinkedList list;
-	Node *head = NULL;
-	Node *tail = NULL;
-	list.pushFront (300,&head, &tail);
-	list.pushFront (50, &head, &tail);
-	list.pushBack (700, &head, &tail);
-	list.pushBack (800, &head, &tail);
+    LinkedList list;
+    Node *head = NULL;
+    Node *tail = NULL;
+    list.pushFront (300,&head, &tail);
+    list.pushFront (50, &head, &tail);
+    list.pushBack (700, &head, &tail);
+    list.pushBack (800, &head, &tail);
     list.pushBack (1000, &head, &tail);
-	list.printList (head);
+    list.printList (head);
     list.popFront(&head, &tail);
     list.popFront(&head, &tail);
     list.printList(head);
@@ -160,5 +160,6 @@ int main ()
     list.printList(head);
     list.deleteList(&head, &tail);
     list.printList (head);
-	return 0;
+	
+    return 0;
 }
