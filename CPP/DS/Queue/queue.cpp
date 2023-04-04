495276b37bed7c069e79195a6dfaaa2745dec1ac
nandu506tvm <nandu506tvm@gmail.com>
2023-04-04 22:33:49 +0530

    QUEUE data structure basic code

---
+++ CPP/DS/Queue/queue.cpp
@@ -0,0 +1,86 @@
+// Implelment queue using circular array 
+
+#include <iostream>
+
+#define MAX_SIZE 5
+
+using namespace std;
+
+class Queue
+{
+    private:
+        int arr[MAX_SIZE] = {0};
+        int current_size = 0;
+        int front = -1;
+        int rear = -1;
+    public:
+        bool isQueueFull()
+        {
+            return current_size == MAX_SIZE;
+        }
+        bool isEmpty()
+        {
+            return current_size == 0;
+        }
+        void enqueue(int data)
+        {
+            if (isQueueFull())
+            {
+                cout << "Queue is full" << endl;
+                return;
+            }
+            if (isEmpty())
+            {
+                front = 0;
+            }
+            rear = (rear+1) % MAX_SIZE;
+            arr[rear] = data;
+            current_size++;
+        }
+        void dequeue()
+        {
+            if (isEmpty())
+            {
+                return;
+            }
+            arr[front] = -1;
+            current_size--;
+            front = (front+1) % MAX_SIZE;
+        }
+        void printArray()
+        {
+            for (int i=0; i<MAX_SIZE; i++)
+            {
+                cout << arr[i] << "\t";
+            }
+            cout << endl;
+        }
+};
+
+int main(int argc, char const *argv[]) 
+{
+    Queue q1;
+    q1.enqueue(1);
+    q1.enqueue(10);
+    q1.enqueue(2);
+    q1.enqueue(3);
+    q1.enqueue(7);
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.printArray();
+    q1.enqueue(11);
+    q1.enqueue(13);
+    q1.enqueue(3);
+    q1.enqueue(5);
+    q1.dequeue();
+    q1.enqueue(50);
+    q1.enqueue(510);
+    q1.dequeue();
+    q1.printArray();
+
+    return 0;
+}
+
---
+++ CPP/DS/Queue/queueStl.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+
+    queue<int> que;
+    que.push(10);
+    que.push(20);
+    que.push(30);
+    que.push(40);
+    que.push(50);
+
+    while(!que.empty())
+    {
+        cout << que.front() << "\t";
+        que.pop();
+    }
+    cout << endl;
+    
+    return 0;
+}
+
