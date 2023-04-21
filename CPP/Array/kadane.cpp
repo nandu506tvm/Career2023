9edd72f0f71bf66069e08d31121dec884c58632b
nandu506tvm <nandu506tvm@gmail.com>
2023-04-21 23:08:33 +0530

    Kadane algorithm for max subarray - O(n)

---
+++ CPP/Array/kadane.cpp
@@ -0,0 +1,39 @@
+// Find the largest subarray sum and return it
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+// Using kadane's algoritm
+int largestSubArraySum(vector<int> list)
+{
+    int current_sum = 0;
+    int max_sum = INT_MIN;
+
+    for(int element: list)
+    {
+        // Add the element to sum
+        current_sum += element;
+        
+        // compare the sum with max_sum
+        max_sum = max(current_sum, max_sum);
+
+        // If sum is negative, then update it to zero - Discarding that negative sub-array
+        if (current_sum < 0) current_sum = 0;
+    }
+
+    return max_sum;
+}
+
+int main(int argc, char const *argv[]) 
+{
+    vector<int> list = {-5,-4,-6,-1,-4,-10};
+
+    cout << largestSubArraySum(list) << endl ;
+    
+    return 0;
+}
+
