38587f4f50d64dc08bb061cc07f79f09bb4a4310
nandu506tvm <nandu506tvm@gmail.com>
2023-04-30 10:53:51 +0530

    Sorting modules added

---
+++ CPP/sorting/sorting.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+
+    vector<int> arr = {10,9,5,8,3,2,11,101,1};
+
+    // Inbuilt sorting algorithm uses 'introsort'
+    // argument 1 -> pointer from where sorting should start in array.
+    // argument 2 -> pointer to one next of last element in array
+    sort(arr.begin(), arr.end());
+
+    // Print the array
+    for (int element: arr)
+    {
+        cout << element << "\t";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
---
+++ CPP/sorting/sorting_comparator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Comparator func to reverse the array.
+bool compare(int a, int b)
+{
+    return a < b;
+}
+
+int main(int argc, char const *argv[]) 
+{
+
+    vector<int> arr = {10,9,5,8,3,2,11,101,1};
+
+    // Inbuilt sorting algorithm uses 'introsort'
+    // argument 1 -> pointer from where sorting should start in array.
+    // argument 2 -> pointer to one next of last element in array
+    sort(arr.begin(), arr.end(), compare);
+
+    // Print the array
+    for (int element: arr)
+    {
+        cout << element << "\t";
+    }
+    cout << endl;
+
+    //do reverse sorting
+    sort(arr.rbegin(), arr.rend());
+
+    for (int element: arr)
+    {
+        cout << element << "\t";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
