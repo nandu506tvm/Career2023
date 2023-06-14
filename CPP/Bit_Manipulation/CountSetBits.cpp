88299d634d3eccd6956c793c4c1e8d8c2b6da786
nandu506tvm <nandu506tvm@gmail.com>
2023-06-14 23:06:17 +0530

    New Bit manipulation scripts added

---
+++ CPP/Bit_Manipulation/CountSetBits.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int CountSetBits(int number) {
+  int bit_mask = 1;
+  int sum = 0;
+
+  while (number != 0) {
+    int right_bit = number & bit_mask;
+    sum += right_bit;
+    number = number >> 1;
+  }
+
+  return sum;
+}
+
+// This new hack works because on each iteration we are removing the last set bit.
+// Number of times the loop iterates will give you the number of set bits.
+int CountSetBitHack(int number) {
+  int sum = 0;
+
+  while (number > 0) {
+    number = number & (number-1);
+    sum ++;
+  }
+
+  return sum;
+}
+
+int main() {
+
+  int number = 0;
+
+  cout << "Enter a number: ";
+  cin >> number;
+
+  cout << CountSetBits(number) << endl;
+
+  return 0;
+}
+
---
+++ CPP/Bit_Manipulation/FastExponentiation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+/* A given number N will have atmost Log (N) number of set bits
+ * For eg: binary of 5 is 000101. Here number of set bits is 2 which is < log (5)
+ * */
+
+
+// Fast exponentiation is a way to calculate the power of a number say 5^2
+// Since we have atmost log N bits in exponent, this algorithm take O(Log N)
+int FastExponentiation(int base, int exponent) {
+    int result = 1;
+
+    // Iterate over the bits of the exponent
+    while (exponent > 0) {
+
+        // LSB is set
+        if (exponent & 1) {
+            result = result * base;
+        }
+
+        // when going for next iteration keep squaring the base
+        base = base * base;
+
+        // keep moving the exponent to the right
+        exponent = exponent >> 1;
+    }
+
+    return result;
+}
+
+int main() {
+
+    cout << FastExponentiation(5,3) << endl;
+
+    return 0;
+}
+
