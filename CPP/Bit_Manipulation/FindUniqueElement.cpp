5e2e5886efb96aa281fa26ebd68f8c67afd8504f
nandu506tvm <nandu506tvm@gmail.com>
2023-06-18 09:46:57 +0530

    Finding unique element using XOR

---
+++ CPP/Bit_Manipulation/FindUniqueElement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Xor of same numbers will be zero. we will use that property
+int FindUniqueElement(vector<int> v) {
+    int result = 0;
+    for (int element: v) {
+        result = result^element;
+    }
+    return result;
+}
+
+int main() {
+    vector<int> vect = {1, 2, 3, 1, 4, 2, 3, 5, 7, 7, 5};
+    cout << FindUniqueElement(vect) << endl;
+
+    return 0;
+}
+
