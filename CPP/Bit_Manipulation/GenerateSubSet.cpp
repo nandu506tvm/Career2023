b1d70caa69d9e1ae0d6bdb86f71c0b3363700549
nandu506tvm <nandu506tvm@gmail.com>
2023-06-18 14:36:39 +0530

    Generate Subset using bit manipulation added

---
+++ CPP/Bit_Manipulation/GenerateSubSet.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Total number of subsets for an array of size N is (2^N) including empty set
+// This bit manipulation alg runs in (N*O(N))
+void GenerateSubSet(vector<int> vect, vector<vector<int>> &result) {
+    int arr_size = vect.size();
+
+    // This is equivalent to doing (2^n)
+    // This will give us the total number of subsets
+    int subset_count = 1<<arr_size;
+
+    // Outer loop for 2^N subset generation 
+    for (int mask=0; mask < subset_count; mask++) {
+        vector<int> subset;
+        
+        // walk bit by bit through the 'mask' and see what all bits are set
+        for (int array_index=0; array_index<arr_size; array_index++) {
+            // If the bit is set, then push the element at that particular index to subset.
+            if (mask & (1<<array_index)) {
+                subset.push_back(vect[array_index]);
+            }
+        }
+        result.push_back(subset);
+    }
+}
+
+int main() {
+    vector <int> vect = {1,2,3,4,5};
+    vector<vector<int>> result;
+    GenerateSubSet(vect,result);
+
+    // Print the result
+    for (auto element: result) {
+        for (auto ele: element) {
+            cout << ele << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
---
+++ CPP/Bit_Manipulation/IsPowerOf2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+// Numbers which are power of 2, have one and only one bit set in binary representation.
+// The position of the bit that is set will give the exponent of 2 that make that number.
+bool IsPowerOfTwo(int number) {
+    int set_bit_count = 0;
+    // Bit at at this position is set
+    while(number >0 ) {
+        if (number & 1) {
+            set_bit_count++;
+        }
+        number >>= 1;
+    }
+    if (set_bit_count>1) {
+        return false;
+    }
+    return true;
+}
+
+bool EasyWay(int number) {
+    bool result = (number & (number-1)) ? false: true;
+    return result;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << IsPowerOfTwo(number) << endl;
+    cout << EasyWay(number) << endl;
+    return 0;
+}
+
