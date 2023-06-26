2622ec79ce7d0a0dd2ab04ce1619a0ea1e5ebead
nandu506tvm <nandu506tvm@gmail.com>
2023-06-26 22:13:17 +0530

    recursion sorting check

--- CPP/recursion/IsSorted.cpp
+++ CPP/recursion/IsSorted.cpp
@@ -2,12 +2,26 @@
 
 using namespace std;
 
-bool IsSorted(vector<int> arr, int end_index) {
+bool IsSorted(vector<int> arr,int start_index, int end_index) {
+    // Base condition
+    if (start_index == end_index) {
+        return true;
+    }
+    // Assume recursion will solve this
+    bool is_remaining_elem_sorted = IsSorted(arr, start_index+1, end_index);
+
+    // Final calculation
+    if (arr[start_index] < arr[start_index+1] and is_remaining_elem_sorted) {
+        return true;
+    }
+
+    // Above check failed
+    return false;
 }
 
 int main() {
     vector<int> arr = {1,2,3,4,5,6,7};
-    cout << IsSorted(arr,arr.size()-1) << endl;
+    cout << IsSorted(arr,0,arr.size()-1) << endl;
     return 0;
 }
 
