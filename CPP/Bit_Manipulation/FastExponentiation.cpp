15dcb28fcaccafaed429c0f9d2180cb0ce6664ca
nandu506tvm <nandu506tvm@gmail.com>
2023-07-15 15:14:17 +0530

    Better code readibilty

--- CPP/Bit_Manipulation/FastExponentiation.cpp
+++ CPP/Bit_Manipulation/FastExponentiation.cpp
@@ -11,14 +11,17 @@ using namespace std;
 // Since we have atmost log N bits in exponent, this algorithm take O(Log N)
 int FastExponentiation(int base, int exponent) {
     int result = 1;
+
     // Iterate over the bits of the exponent
     while (exponent > 0) {
         // LSB is set
         if (exponent & 1) {
             result = result * base;
         }
+
         // when going for next iteration keep squaring the base
         base = base * base;
+
         // keep moving the exponent to the right
         exponent = exponent >> 1;
     }
