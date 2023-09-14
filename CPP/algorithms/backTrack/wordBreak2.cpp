1db65559d08c5bc220e446445cde8c55cf711c41
nandu506tvm <nandu506tvm@gmail.com>
2023-09-14 22:17:10 +0530

    Minor changes added

--- CPP/algorithms/backTrack/wordBreak2.cpp
+++ CPP/algorithms/backTrack/wordBreak2.cpp
@@ -15,7 +15,7 @@ i like samsung mobile
 
 using namespace std;
 
-int wordBreak(string s, vector<string>& wordDict, string& out_str, int start_idx) {
+int wordBreak(string s, vector<string>& wordDict, int start_idx) {
     // Base condition
     if (start_idx >= s.length()) {
         return 1;
@@ -29,7 +29,7 @@ int wordBreak(string s, vector<string>& wordDict, string& out_str, int start_idx
 
         // Element found in word Dict
         if (word_in_dict != wordDict.end()) {
-            total_ways += wordBreak(s, wordDict, out_str, i + 1);
+            total_ways += wordBreak(s, wordDict, i + 1);
         }
     }
     return total_ways;
@@ -38,9 +38,8 @@ int wordBreak(string s, vector<string>& wordDict, string& out_str, int start_idx
 int main(int argc, char const* argv[]) {
     vector<string> dictionary = {"i", "love", "sam", "sung", "samsung"};
     string str = "ilovesamsung";
-    string out_str = "";
 
-    cout << wordBreak(str, dictionary, out_str, 0) << endl;
+    cout << wordBreak(str, dictionary, 0) << endl;
 
     return 0;
 }
