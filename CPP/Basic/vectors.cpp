02ae3b58fb7b10d29448ab62775cd911bd76765f
Nandu J B <80139351+nandu506tvm@users.noreply.github.com>
2024-08-15 17:09:47 +0530

    Update vectors.cpp
    Code added to calculate the capacity of the vector

--- CPP/Basic/vectors.cpp
+++ CPP/Basic/vectors.cpp
@@ -34,6 +34,11 @@ int main(int argc, char const *argv[]) {
     salary.push_back(3000);
     salary.push_back(1000);
     salary.push_back(2000);
+
+    // This will show the current capacity of the vector
+    cout << salary.capacity() << endl;
+
+    // Call the function to calculate the average
     cout << average(salary) << endl;
 
     return 0;
